a8c30196776adc93098befe4a6269881e022807b
anugrahSA <anugrahnugi321@gmail.com>
2022-03-18 16:05:46 +0800

    menambahkan class DataGeneric

--- tpmodul4_1302204047/Program.cs
+++ tpmodul4_1302204047/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_130
+{
+
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("Halo User " + x);
+        }
+    }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            String x = "Anugrah Saputra Aras";
+            halo.SapaUser(x);
+            DataGeneric<String> data = new DataGeneric<string>("1302204047");
+            data.PrintData();
+        }
+    }
+}
